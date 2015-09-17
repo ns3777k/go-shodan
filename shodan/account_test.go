2b3cc00e295baea07b3b4ed999490b9d53a92579
Safonov Nikita <ns3777k@gmail.com>
2015-09-17 05:03:37 +0300

    add account profile test

---
+++ shodan/account_test.go
@@ -0,0 +1,30 @@
+package shodan
+
+import (
+	"testing"
+	"net/http"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_GetAccountProfile(t *testing.T) {
+	setUpTestServe()
+	defer tearDownTestServe()
+
+	mux.HandleFunc(profilePath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.Write(getStub(t, "profile"))
+	})
+
+	account, err := client.GetAccountProfile()
+	accountExpected := &Profile{
+		Member: true,
+		Name: "",
+		Credits: 40,
+		Created: "2015-09-03T12:44:29.278000",
+	}
+
+	assert.Nil(t, err);
+	assert.IsType(t, accountExpected, account)
+	assert.EqualValues(t, accountExpected, account)
+}
