d1cc055edeedc117fdb74aa8098a75d0b6f4c22a
Safonov Nikita <ns3777k@gmail.com>
2015-09-19 03:02:02 +0300

    ports test

---
+++ shodan/ports_test.go
@@ -0,0 +1,25 @@
+package shodan
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_GetPorts(t *testing.T) {
+	setUpTestServe()
+	defer tearDownTestServe()
+
+	mux.HandleFunc(portsPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.Write(getStub(t, "ports"))
+	})
+
+	portsExpected := []int{22, 771, 5353, 110, 8139}
+	ports, err := client.GetPorts()
+
+	assert.Nil(t, err)
+	assert.Len(t, ports, len(portsExpected))
+	assert.EqualValues(t, portsExpected, ports)
+}
